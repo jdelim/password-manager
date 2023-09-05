ef3d36733ee49d6ea443288865661d824f063e8a
Jadriel Delim <jadrieldelim@gmail.com>
2023-09-05 12:40:46 -0700

    added gitignore and .ini files

--- database.py
+++ database.py
@@ -6,8 +6,13 @@ import psycopg2
 def establish_conn(database, user, password, host, port):
     conn = psycopg2.connect(database = database, user = user, password = 
                             password, host = host, port = port)
+    conn.autocommit = True
     
-
+# create db based on user's username
+# FIXME - usernames must be unique
 def create_database(name):
-    # take user input for name of database
+
+
+def create_table():
+    
     
